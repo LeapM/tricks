a8ee04da2e4514ff4a2513c4ccbe8d153ba51b8f
Wang <Luming.Wang@jacobs.com>
2017-11-30 16:16:29 +0800

    add solidity

---
+++ btcmining/pool.md
@@ -0,0 +1,3 @@
+1. difficulty(D)
+
+every comptuted hash will lead to valid block with probablility 1/2**32D
---
+++ solidity/good sample/staticcalldynamicalcall.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.13;
+
+contract Receiver {
+    event Log(address addr, uint val);
+    function onTokenReceived(address from, uint val) 
+    returns (bool) {
+        Log(from, val);
+        return true;
+    }
+}
+
+contract FallbackReceiver {
+    event Log(uint x);
+    function () {
+        Log(uint8(msg.data[0]));
+    }
+}
+
+contract NonReceiver {}
+
+contract Sender {
+    Receiver rec = new Receiver();
+    address fallbackRec = address(new FallbackReceiver());
+    address nonRec = address(new NonReceiver());
+    address a = 0x1;
+    uint u = 1;
+
+    function test1() {
+        rec.onTokenReceived(a, u);
+    }
+    function test2() {
+        address(rec).call(bytes4(sha3("onTokenReceived(address,uint256)")), a, u);
+    }
+    function test3() {
+        Receiver(fallbackRec).onTokenReceived(a, u);
+    } 
+    function test4() {
+        fallbackRec.call(bytes4(sha3("onTokenReceived(address,uint256)")), a, u);
+    } 
+    function test5() {
+        Receiver(nonRec).onTokenReceived(a, u);
+    }
+    function test6() {
+        require(nonRec.call(bytes4(sha3("onTokenReceived(address,uint256)")), a, u));
+    } 
+}
